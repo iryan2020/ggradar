04df3ed9f4cd11b3ee2bd58958632c898d48048b
MUGUMAARHAHAMA Yannick <35613951+iryan2020@users.noreply.github.com>
2024-04-29 15:08:47 +0100

    ggradar.R

--- R/ggradar.R
+++ R/ggradar.R
@@ -121,7 +121,7 @@ ggradar <- function(plot.data,
     x = plot.data[, -1], 
     by = list(plot.data[, 1]), 
     FUN = "mean")
-    
+  
   if (!is.factor(plot.data[, 1])) {
     plot.data[, 1] <- as.factor(as.character(plot.data[, 1]))
   }
@@ -129,11 +129,11 @@ ggradar <- function(plot.data,
   var.names <- colnames(plot.data)[-1] # Short version of variable names
   # axis.labels [if supplied] is designed to hold 'long version' of variable names
   # with line-breaks indicated using \n
-
+  
   # calculate total plot extent as radius of outer circle x a user-specifiable scaling factor
   plot.extent.x <- (grid.max + abs(centre.y)) * plot.extent.x.sf
   plot.extent.y <- (grid.max + abs(centre.y)) * plot.extent.y.sf
-
+  
   # Check supplied data makes sense
   if (length(axis.labels) != ncol(plot.data) - 1) {
     stop("'axis.labels' contains the wrong number of axis labels", call. = FALSE)
@@ -158,7 +158,7 @@ ggradar <- function(plot.data,
   # (b) convert into radial coords
   group <- NULL
   group$path <- CalculateGroupPath(plot.data.offset)
-
+  
   # print(group$path)
   # (c) Calculate coordinates required to plot radial variable axes
   axis <- NULL
@@ -207,7 +207,7 @@ ggradar <- function(plot.data,
   # print(gridline$max$label)
   # print(gridline$mid$label)
   ### Start building up the radar plot
-
+  
   # Declare 'theme_clear', with or without a plot legend as required by user
   # [default = no legend if only 1 group [path] being plotted]
   theme_clear <- theme_bw(base_size = base.size) +
@@ -220,9 +220,9 @@ ggradar <- function(plot.data,
       panel.border = element_blank(),
       legend.key = element_rect(linetype = "blank")
     )
-
+  
   if (plot.legend == FALSE) legend.position <- "none"
-
+  
   # Base-layer = axis labels + plot extent
   # [need to declare plot extent as well, since the axis labels don't always
   # fit within the plot area automatically calculated by ggplot, even if all
@@ -232,7 +232,7 @@ ggradar <- function(plot.data,
   # [abs(x) < x.centre.range]; then left-justified axis labels to right of Y axis [x>0].
   # This building up the plot in layers doesn't allow ggplot to correctly
   # identify plot extent when plotting first (base) layer]
-
+  
   # base layer = axis labels for axes to left of central y-axis [x< -(x.centre.range)]
   base <- ggplot(axis$label) +
     xlab(NULL) +
@@ -244,7 +244,7 @@ ggradar <- function(plot.data,
     ) +
     scale_x_continuous(limits = c(-1.5 * plot.extent.x, 1.5 * plot.extent.x)) +
     scale_y_continuous(limits = c(-plot.extent.y, plot.extent.y))
-
+  
   # ... + circular grid-lines at 'min', 'mid' and 'max' y-axis values
   base <- base + geom_path(
     data = gridline$min$path, aes(x = x, y = y),
@@ -258,7 +258,7 @@ ggradar <- function(plot.data,
     data = gridline$max$path, aes(x = x, y = y),
     lty = gridline.max.linetype, colour = gridline.max.colour, linewidth = grid.line.width
   )
-
+  
   # + axis labels for any vertical axes [abs(x)<=x.centre.range]
   base <- base + geom_text(
     data = subset(axis$label, abs(axis$label$x) <= x.centre.range),
@@ -277,15 +277,15 @@ ggradar <- function(plot.data,
     fill = background.circle.colour,
     alpha = background.circle.transparency
   )
-
+  
   # + radial axes
   base <- base + geom_path(
     data = axis$path, aes(x = x, y = y, group = axis.no),
     colour = axis.line.colour
   )
-
+  
   theGroupName <- names(group$path[1])
-
+  
   # ... + group (cluster) 'paths'
   # base <- base + geom_path(
   #   data = group$path, aes(x = .data[["x"]], y = .data[["y"]], group = theGroupName, colour = theGroupName),
@@ -299,7 +299,7 @@ ggradar <- function(plot.data,
     base <- base + geom_path(data = group$path, aes(x = .data[["x"]], y = .data[["y"]], group = .data[[theGroupName]], colour = .data[[theGroupName]]), linewidth = group.line.width) +
       scale_alpha_manual(values = line.alpha)
   }
-
+  
   # ... + group points (cluster data)
   # Modify point drawing logic based on draw.points
   if (draw.points) {
@@ -313,16 +313,16 @@ ggradar <- function(plot.data,
         scale_alpha_manual(values = point.alpha)
     }
   }
-
+  
   # ... + group (cluster) fills
   if (fill == TRUE) {
     base <- base + geom_polygon(data = group$path, aes(x = .data[["x"]], y = .data[["y"]], group = .data[[theGroupName]], fill = .data[[theGroupName]]), alpha = fill.alpha)
   }
-
-
+  
+  
   # ... + amend Legend title
   if (plot.legend == TRUE) base <- base + labs(colour = legend.title, size = legend.text.size)
-
+  
   # ... + grid-line labels (max; mid; min)
   if (label.gridline.min == TRUE) {
     base <- base + geom_text(aes(x = x, y = y, label = values.radar[1]), data = gridline$min$label, size = grid.label.size * 0.8, hjust = 1, family = font.radar)
@@ -338,13 +338,13 @@ ggradar <- function(plot.data,
     centre.y.label <- data.frame(x = 0, y = 0, text = as.character(centre.y))
     base <- base + geom_text(aes(x = x, y = y, label = text), data = centre.y.label, size = grid.label.size, hjust = 0.5, family = font.radar)
   }
-
+  
   if (!is.null(group.colours)) {
     colour_values <- rep(group.colours, length(unique(plot.data[, 1])) / length(group.colours))
   } else {
     colour_values <- generate_color_values(length(unique(plot.data[, 1])))
   }
-
+  
   base <- base +
     theme(
       legend.key.width = unit(3, "line"),
@@ -358,20 +358,20 @@ ggradar <- function(plot.data,
     scale_colour_manual(values = colour_values) +
     theme(text = element_text(family = font.radar)) +
     theme(legend.title = element_blank())
-
-
+  
+  
   if (isTRUE(fill)) {
     base <- base +
       scale_fill_manual(values = colour_values, guide = "none")
   }
-
+  
   if (legend.title != "") {
     base <- base + theme(legend.title = element_text())
   }
-
+  
   if (plot.title != "") {
     base <- base + ggtitle(plot.title)
   }
-
+  
   return(base)
 }
